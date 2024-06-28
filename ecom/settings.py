50fdfb112aa910f43a019eed3675eb73c84ca8e7
Thasni Abbas <thasniabbas76@gmail.com>
2024-06-28 20:07:45 +0530

    store and cart functionalities

--- ecom/settings.py
+++ ecom/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'accounts',
-    'store'
+    'store',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # 'category.context_processors.menu_links',
+                'cart.context_processors.counter',
             ],
         },
     },
