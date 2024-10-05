04793b8b9044e55affdeb9d78b3243b4c0f4a6a7
Thasni Abbas <thasniabbas76@gmail.com>
2024-10-05 17:16:04 +0530

    set up user dashboard, my orders, edit profile and its functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 # from django.http import HttpResponseRedirect
 # from django.urls import reverse
-from .forms import RegistrationForm
-from .models import Account
+from .forms import RegistrationForm, UserForm, UserProfileForm
+from .models import Account, UserProfile
 from django.contrib import messages,auth
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
@@ -10,6 +10,7 @@ from django.http import HttpResponse
 from cart.models import Cart, CartItem
 from cart.views import _cart_id
 import requests
+from orders.models import Order
 
 #verification email
 from django.contrib.sites.shortcuts import get_current_site
@@ -114,7 +115,12 @@ def activate(request, uidb64,token):
         return redirect('register')
 @login_required(login_url = 'login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id,is_ordered = True)
+    orders_count = orders.count()
+    context = {
+        'orders_count' : orders_count,
+    }
+    return render(request, 'accounts/dashboard.html',context)
 def forgotPassword(request):
     if request.method == "POST":
         email = request.POST['email']
@@ -170,3 +176,29 @@ def resetPassword(request):
             return redirect('resetPassword')
     else:
         return render(request, 'accounts/resetPassword.html')
+
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user,is_ordered = True).order_by('-created_at') #using '-' to get the result in descending order
+    context = {
+        'orders' : orders,
+    }
+    return render(request, 'accounts/my_orders.html',context)
+def edit_profile(request):
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST,instance=request.user) #instance is used to update/edit the profile
+        profile_form = UserProfileForm(request.POST,request.FILES,instance=userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Your profile has been updated.")
+            return redirect('edit_profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=userprofile)
+    context = {
+        'user_form' : user_form,
+        'profile_form' : profile_form,
+        'userprofile' : userprofile,
+    }
+    return render(request, 'accounts/edit_profile.html', context)
