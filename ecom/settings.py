d02304a891aa5a88b397a7b6fe48c7a4c4911c45
Thasni Abbas <thasniabbas76@gmail.com>
2024-11-14 11:14:36 +0530

    added package config

--- ecom/settings.py
+++ ecom/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default = True, cast=bool) #True
 
-ALLOWED_HOSTS = ['furni-env.eba-hcvdbgmw.ap-south-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['ecom-env.eba-epvvbktd.ap-south-1.elasticbeanstalk.com']
 
 
 # Application definition
