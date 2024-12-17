33d4e025d4b7b48701cd63af1f1f9ba935eeb481
Thasni Abbas <thasniabbas76@gmail.com>
2024-12-17 18:38:22 +0530

    static file settings

--- ecom/urls.py
+++ ecom/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf import settings #new
+from django.conf.urls.static import static #new
 
 urlpatterns = [
     path('securelogin/', admin.site.urls),
@@ -30,3 +32,7 @@ urlpatterns = [
     #orders
     path('orders/', include('orders.urls')),
 ] +static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
