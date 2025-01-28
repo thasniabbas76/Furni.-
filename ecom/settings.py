0fe508be37618c0509f4cc8b145128864e07d21d
Thasni Abbas <thasniabbas76@gmail.com>
2025-01-28 23:35:45 +0530

    added allowed host on render

--- ecom/settings.py
+++ ecom/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default = True, cast=bool) #True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://furni-74a5.onrender.com']
 
 # Application definition
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
