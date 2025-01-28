0d7c3294f26632fcf5d9012ebee2cb1c27a725d0
Thasni Abbas <thasniabbas76@gmail.com>
2025-01-28 23:55:07 +0530

    updated allowed host again

--- ecom/settings.py
+++ ecom/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default = True, cast=bool) #True
 
-ALLOWED_HOSTS = ['https://furni-74a5.onrender.com']
+ALLOWED_HOSTS = ['localhost','furni-74a5.onrender.com','0.0.0.0']
 
 # Application definition
 
